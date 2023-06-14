27a455d7bb96f48aedc69db8c3fde03f32ae1dc6
NobodyRetro <59867447+NobodyRetro@users.noreply.github.com>
2023-06-14 01:03:15 -0700

    Add files via upload

---
+++ Hummingbird/Program.cs
@@ -0,0 +1,9 @@
+﻿/* Anna's Hummingbird, now believed to be the fastest bird in the world relative to its size can reach speeds of 80km per hour.
+Write a program to output how many KPH it will travel each hour over 5 hours of flight.*/
+
+for (int i = 1 ; i <= 5 ; i++)
+{
+   Console.WriteLine(i*80);
+}
+Console.WriteLine("Press any key to exit");
+Console.ReadKey();
